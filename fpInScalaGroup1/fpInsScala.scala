7549f4b73f22bdfc7216fac4d0c186f77ef6b51b
svenski <sergiuszbleja@gmail.com>
2013-05-10 12:03:45 +0200

    Adding April dojo

--- fpInScalaGroup1/fpInsScala.scala
+++ fpInScalaGroup1/fpInsScala.scala
@@ -1,4 +1,3 @@
-
 object chapter3 extends App {
 
   // Exercise 2
@@ -38,7 +37,59 @@ object chapter3 extends App {
   println(dropWhile(List(1,2,3,4,5)) ((x: Int) => x < 3))
 
   def lessThanThree(n : Int) = n < 3
-        
+
+  // Exercise 5
+  def setHead[T](l: List[T], newHead: T) = {
+    newHead :: tail(l)
+  }
+  
+  // Exercise 6  
+  def init[A](l: List[A]) : List[A] = l match {
+      case Nil => Nil
+      case _ :: Nil => Nil
+      case x :: xs => x::init(xs)
+  }                                               //> init: [A](l: List[A])List[A]
+
+  // Exercise 10
+  def length[A](l: List[A]): Int = l.foldRight(0)((_, c) => c + 1)
+
+  val dropWhileForThisList = dropWhile( List(33345,4,5,3)) _
+
+  println(dropWhileForThisList( _  > 4))
+
+  println(tail(List(1,2, 3)))
+  println(tail(List()))
+
+  println(drop(List(), 2))
+  println(drop(List(1,2, 3), 2))
+  println(drop(List(1,2, 3), 4))
+
+
+  println(dropWhile(List(1,2,3,4,5)) ((x: Int) => x < 3))
+
+  def lessThanThree(n : Int) = n < 3
+  
+  // Exercise 11
+  @tailrec
+  def foldLeft[A,B] (l: List[A], z: B)(f: (B,A) => B): B = {
+    l match {
+      case Nil => z
+      case x :: xs => foldLeft( xs, f(z, x))(f)
+    }
+  }
+  
+  // Exercise 13
+  def reverse[T](l: List[T]): List[T] = {
+    foldLeft(l, Nil : List[T])( (acc, x) => x :: acc)
+  }
+  
+  // Exercise 14
+  def foldLeft2[A,B](l: List[A], z:B)(f: (B,A) =>B): B = {
+    def ident(x: B): B = x
+    val g = l.foldRight(ident _)((e, a) => x => a(f(x, e)))
+    g(z)
+  }
+          
 
 /////////////////////////////
 // May Dojo
